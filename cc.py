7fee20e3126824e4187b6291f81c34a62c7e135b
MaciejKrzysiak <krzysiakmaciej159@gmail.com>
2021-04-05 19:31:07 -0500

    Add Help Messages and Setup Main Functionality. Added .json Settings File

--- cc.py
+++ cc.py
@@ -1,32 +1,52 @@
 import sys
 import getopt
+import json
 import cryptocompare
 
-def main(source, amount, destination):
-    rate = cryptocompare.get_price(source, currency=destination, full=True)
-    rate_at_amount = rate * amount
-    print(rate_at_amount)
+default_currency = 'USD'
 
+def help():
+    print('cc.py -s <source> -a <amount> -d <destination> -h <help> -l <list>')
+    print('Example Usage: python3 cc.py -s BTC -a 1.01 -d USD')
+    print('A sample of supported currencies are listed below.')
+    print('To see them all, use the -l or --list flag.')
+    print('[NANO, BTC, ETH, XRP, USD, GBP]')
+
+def main(source, amount, destination):
+    rate_at_amount = None
+    if (source and amount and destination):
+        rate = cryptocompare.get_price(source, currency=destination)
+        rate_at_amount = float(rate[source][destination]) * float(amount)
+    elif (source and amount):
+        rate = cryptocompare.get_price(source, currency=default_currency)
+        rate_at_amount = float(rate[source][destination]) * float(amount)
+    print(rate_at_amount)
 
 if __name__ == "__main__":
 
-    cryptocompare.cryptocompare._set_api_key_parameter("KEY_HERE")
+    f = open('super_secret.json',)
+    data = json.load(f)
+    secret_key = data['secret_key']
+    cryptocompare.cryptocompare._set_api_key_parameter("secret_key")
 
+    opts = None
     try:
-      opts, args = getopt.getopt(argv,"hs:a:d:",["source=","destination="])
+        opts, args = getopt.getopt(sys.argv[1:],"hls:a:d:",["help","list","source=","amount=","destination=",])
     except getopt.GetoptError:
-      print 'cc.py -s <source> -a <amount> -d <destination>'
-      print 'See supported currencies in currencies.json'
-      sys.exit(2)
+        help()
+        sys.exit(2)
 
     source = None
     amount = None
     destination = None
+    list_of_currencies = cryptocompare.get_coin_list(format=True) + ['USD', 'GBP', 'EUR']
     for opt, arg in opts:
         if opt == '-h':
-            print 'cc.py -s <sourceCurrency> -a <amount> -d <destinationCurrency>'
-            print 'See supported currencies in currencies.json'
+            help()
             sys.exit()
+        if opt == '-l':
+            print('All supported currencies are:')
+            print(list_of_currencies)
         elif opt in ("-s", "--source"):
             source = arg
         elif opt in ("-a", "--amount"):
@@ -34,4 +54,11 @@ if __name__ == "__main__":
         elif opt in ("-d", "--destination"):
              destination = arg
 
-    main(souce, amount, destination)
+    valid_source = source in list_of_currencies
+    valid_amount = amount.isdigit()
+    valid_destination = (destination is None) or (destination in list_of_currencies)
+    if (valid_source and valid_amount and valid_destination):
+        main(source, amount, destination)
+    else:
+        print('Your command line arguments are not valid.')
+        help()
