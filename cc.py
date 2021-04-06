42cb29bedad301351718c44386f1f2f5a24d1777
MaciejKrzysiak <krzysiakmaciej159@gmail.com>
2021-04-05 19:44:22 -0500

    Linted and Commented. RTG

--- cc.py
+++ cc.py
@@ -3,35 +3,55 @@ import getopt
 import json
 import cryptocompare
 
-default_currency = 'USD'
+# Set your default currency. Supports USD, GBP, EUR and others.
+# See https://www.cryptocompare.com/ for more possible options.
+default_currency = "USD"
+
 
 def help():
-    print('cc.py -s <source> -a <amount> -d <destination> -h <help> -l <list>')
-    print('Example Usage: python3 cc.py -s BTC -a 1.01 -d USD')
-    print('A sample of supported currencies are listed below.')
-    print('To see them all, use the -l or --list flag.')
-    print('[NANO, BTC, ETH, XRP, USD, GBP]')
+    print("cc.py -s <source> -a <amount> -d <destination> -h <help> -l <list>")
+    print("Example Usage: python3 cc.py -s BTC -a 1.01 -d USD")
+    print("A sample of supported currencies are listed below.")
+    print("To see them all, use the -l or --list flag.")
+    print("[NANO, XMR, BTC, ETH, XRP, USD, GBP]")
+
 
 def main(source, amount, destination):
-    rate_at_amount = None
-    if (source and amount and destination):
+    if source and amount and destination:
         rate = cryptocompare.get_price(source, currency=destination)
         rate_at_amount = float(rate[source][destination]) * float(amount)
-    elif (source and amount):
+        print(
+            f"{amount} {source} converted to {destination} is valued at {rate_at_amount} {destination}"
+        )
+    elif source and amount:
         rate = cryptocompare.get_price(source, currency=default_currency)
-        rate_at_amount = float(rate[source][destination]) * float(amount)
-    print(rate_at_amount)
+        rate_at_amount = float(rate[source][default_currency]) * float(amount)
+        print(f"{amount} {source} is valued at {rate_at_amount} {default_currency}")
+
 
 if __name__ == "__main__":
 
-    f = open('super_secret.json',)
+    # Please generate a free key at:
+    # https://www.cryptocompare.com/cryptopian/api-keys.
+    f = open(
+        "super_secret.json",
+    )
     data = json.load(f)
-    secret_key = data['secret_key']
+    secret_key = data["secret_key"]
     cryptocompare.cryptocompare._set_api_key_parameter("secret_key")
-
     opts = None
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hls:a:d:",["help","list","source=","amount=","destination=",])
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "hls:a:d:",
+            [
+                "help",
+                "list",
+                "source=",
+                "amount=",
+                "destination=",
+            ],
+        )
     except getopt.GetoptError:
         help()
         sys.exit(2)
@@ -39,26 +59,31 @@ if __name__ == "__main__":
     source = None
     amount = None
     destination = None
-    list_of_currencies = cryptocompare.get_coin_list(format=True) + ['USD', 'GBP', 'EUR']
+    list_of_currencies = cryptocompare.get_coin_list(format=True) + [
+        "USD",
+        "GBP",
+        "EUR",
+    ]
     for opt, arg in opts:
-        if opt == '-h':
+        if opt == "-h":
             help()
             sys.exit()
-        if opt == '-l':
-            print('All supported currencies are:')
+        if opt == "-l":
+            print("All supported currencies are:")
             print(list_of_currencies)
+            sys.exit()
         elif opt in ("-s", "--source"):
             source = arg
         elif opt in ("-a", "--amount"):
             amount = arg
         elif opt in ("-d", "--destination"):
-             destination = arg
+            destination = arg
 
     valid_source = source in list_of_currencies
     valid_amount = amount.isdigit()
     valid_destination = (destination is None) or (destination in list_of_currencies)
-    if (valid_source and valid_amount and valid_destination):
+    if valid_source and valid_amount and valid_destination:
         main(source, amount, destination)
     else:
-        print('Your command line arguments are not valid.')
+        print("Your command line arguments are not valid.")
         help()
